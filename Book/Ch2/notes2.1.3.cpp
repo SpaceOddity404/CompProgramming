746245523ce1138bf874a814cccff81abe04ec7c
Nithya <jaynithya123@gmail.com>
2021-01-05 11:12:55 -0500

    2.1.3 notes

---
+++ Book/Ch2/notes2.1.3.cpp
@@ -0,0 +1,27 @@
+// Before running the code, add stdc++.h to your local computer and specify the file path in the #include header.
+#include </Users/nithya/bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    /*SHORTERNING CODE*/
+        //typedef can be used to give a short name to a data type.
+        typedef long long ll;
+        ll a = 123456789;
+        ll b = 987654321;
+        cout << a*b << "\n";
+
+        //A macro specifies that certain strings in the code will be changed before compilation.
+        #define F first
+        #define S second
+        
+
+
+
+
+    return 0;
+
+}
